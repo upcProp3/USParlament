f6d20629b958c303afbd35aa50f3f110cf9e6016
ondrej <ondrejvelisek@gmail.com>
2015-04-15 11:30:51 +0200

    Add CommunityAlgorithm and CommunitySet classes

---
+++ src/es/upc/fib/prop/shared13/CommunityAlgorithm.java
@@ -0,0 +1,10 @@
+package es.upc.fib.prop.shared13;
+
+/**
+ * Created by ondrej on 15.4.15.
+ */
+public abstract class CommunityAlgorithm {
+
+	public abstract CommunitySet compute(Graph graph);
+
+}
---
+++ src/es/upc/fib/prop/shared13/CommunitySet.java
@@ -0,0 +1,8 @@
+package es.upc.fib.prop.shared13;
+
+/**
+ * Created by ondrej on 15.4.15.
+ */
+public class CommunitySet {
+
+}
