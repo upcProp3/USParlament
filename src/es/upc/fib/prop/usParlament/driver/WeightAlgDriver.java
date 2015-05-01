b80bf916222a2521c05091b76c7526e57f0f1008
aleixsacrest <aleixsacrest@gmail.com>
2015-05-01 13:29:59 +0200

    Changes in MP+WeightAlg+WeightAlg Driver

--- src/es/upc/fib/prop/usParlament/driver/WeightAlgDriver.java
+++ src/es/upc/fib/prop/usParlament/driver/WeightAlgDriver.java
@@ -52,12 +52,13 @@ public class WeightAlgDriver {
         Scanner read = new Scanner(System.in);
         while(seguir) {
             System.out.println("Node Management:");
-            System.out.println("1-Enter MP's\n2-Erase MP\n3- Get all MP's\n4--Get Common Attributes\nany other key-EXIT");
+            System.out.println("1-Enter MP's\n2-Erase MP\n3-Get an MP with its attributes\n4-Get all MP's\n5-Get Common Attributes\nany other key-EXIT");
             Integer num = read.nextInt();
             String fullname;
             String st;
             State estat;
             Integer dist;
+            MP p1;
             switch(num) {
                 case 1:
                     System.out.println("Enter the full name, the state and the district of the MP's\nafter entering anything hit enter\nwhen you're done instead of the name");
@@ -80,41 +81,47 @@ public class WeightAlgDriver {
                     //TODO
                     break;
                 case 3:
+                    System.out.println("Enter the MP state and district whose info you want to show:");
+                    System.out.print("state: ");
+                    st = read.next();
+                    estat = State.valueOf(st);
+                    System.out.print("district: ");
+                    dist = read.nextInt();
+                    read.nextLine();
+                    p1 = null;
+                    p1 = c.getMP(estat, dist);
+                    if (p1.equals(null)) {
+                        System.out.println("there's not such MP in the congress");
+                        break;
+                    }
+                    System.out.println(p1);
+                    System.out.println('\n');
+                    break;
+                case 4:
                     System.out.println("These are all MP's in the congress:");
                     Collection<MP> mps = c.getMPs();
                     for (MP p : mps) System.out.println(p);
                     System.out.println();
                     break;
-                case 4:
+                case 5:
                     System.out.println("Enter the MP's you want to compare:");
-                    System.out.print("full name1: ");
-                    read.nextLine();
-                    fullname = read.nextLine();
                     System.out.print("state1: ");
                     st = read.next();
                     estat = State.valueOf(st);
                     System.out.print("district1: ");
                     dist = read.nextInt();
-                    MP p1 = null;
+                    p1 = null;
+                    p1 = c.getMP(estat, dist);
                     Boolean found = false;
-                    for (MP p : c.getMPs())
-                        if (p.getFullname().equals(fullname) && p.getState().equals(estat) && p.getDistrict() == dist) {p1 = p; found = true;}
-                    if (!found) {System.out.println("there's not such MP in the congress"); break;}
-                    System.out.print("full name2: ");
-                    read.nextLine();
-                    fullname = read.nextLine();
+                    if (p1.equals(null)) {System.out.println("there's not such MP in the congress"); break;}
                     System.out.print("state2: ");
                     st = read.next();
                     estat = State.valueOf(st);
                     System.out.print("district2: ");
                     dist = read.nextInt();
                     MP p2 = null;
-                    for (MP p : c.getMPs())
-                        if (p.getFullname().equals(fullname) && p.getState().equals(estat) && p.getDistrict() == dist) {
-                            p2 = p;
-                            found = true;
-                        }
-                    if (!found) {System.out.println("there's not such MP in the congress"); break;}
+                    p2 = c.getMP(estat, dist);
+                    if (p2.equals(null)) {System.out.println("there's not such MP in the congress"); break;}
                     for (Attribute a : wa.getCommonAttributes(p1, p2)) System.out.println(a);
                     System.out.println();
                     break;
@@ -132,6 +139,7 @@ public class WeightAlgDriver {
             System.out.println("1-Add Attributes\n2-Add new type of attribute\n3-Delete Attribute\n4-Change the value of an attribute\n" +
                     "5-Get the importance of a type of attributes\n6-Set importance to a type of attributes\nany other key-EXIT");
             Integer num = read.nextInt();
+            read.nextLine();
             String fullname;
             String st;
             State estat;
@@ -141,29 +149,23 @@ public class WeightAlgDriver {
             switch(num) {
                 case 1:
                     System.out.println("Enter the MP's to whom you want to add attributes:");
-                    System.out.print("full name: ");
-                    read.nextLine();
-                    fullname = read.nextLine();
                     System.out.print("state: ");
                     st = read.next();
                     estat = State.valueOf(st);
                     System.out.print("district: ");
                     dist = read.nextInt();
+                    read.nextLine();
                     p1 = null;
-                    for (MP p : c.getMPs())
-                        if (p.getFullname().equals(fullname) && p.getState().equals(estat) && p.getDistrict() == dist) {
-                            p1 = p;
-                            break;
-                        }
+                    p1 = c.getMP(estat, dist);
                     if (p1.equals(null)) {
                         System.out.println("there's not such MP in the congress");
                         break;
                     }
-                    System.out.println("Enter the info of the attributes you want to add to " + fullname + " (type & value");
+                    System.out.println("Enter the info of the attributes you want to add to " + p1.getFullname() + " (type & value");
                     System.out.println("when you're done enter 0 insted of the type");
                     while (true) {
                         System.out.print("type of attribute: ");
-                        read.nextLine();
+                        //read.nextLine();
                         String d = read.nextLine();
                         def = c.getAttrDef(d);
                         if (d.equals("0")) break;
@@ -178,69 +180,58 @@ public class WeightAlgDriver {
                 case 2:
                     System.out.println("Enter the type info");
                     System.out.print("name: ");
-                    read.nextLine();
+                    //read.nextLine();
                     String name = read.nextLine();
                     System.out.print("importance: ");
                     Integer importance = read.nextInt();
+                    read.nextLine();
                     def = new AttrDefinition(name, importance);
                     c.addAttrDef(def);
+                    System.out.println('\n');
                     break;
                 case 3:
                     System.out.println("Enter the MP's to whom you want to delete an attribute:");
-                    System.out.print("full name: ");
-                    read.nextLine();
-                    fullname = read.nextLine();
                     System.out.print("state: ");
                     st = read.next();
                     estat = State.valueOf(st);
                     System.out.print("district: ");
                     dist = read.nextInt();
+                    read.nextLine();
                     p1 = null;
-                    for (MP p : c.getMPs())
-                        if (p.getFullname().equals(fullname) && p.getState().equals(estat) && p.getDistrict() == dist) {
-                            p1 = p;
-                            break;
-                        }
+                    p1 = c.getMP(estat, dist);
                     if (p1.equals(null)) {
                         System.out.println("there's not such MP in the congress");
                         break;
                     }
-                    System.out.println("Enter the info of the attribute you want to delete of " + fullname + " (type & value");
+                    System.out.println("Enter the info of the attribute you want to delete of " + p1.getFullname() + " (type & value");
                     System.out.print("type of attribute: ");
-                    read.nextLine();
+                    //read.nextLine();
                     def = c.getAttrDef(read.nextLine());
                     p1.removeAttribute(def);
                     break;
                 case 4:
                     System.out.println("Enter the MP's to whom you want to change the value of an attribute:");
-                    System.out.print("full name: ");
-                    read.nextLine();
-                    fullname = read.nextLine();
                     System.out.print("state: ");
                     st = read.next();
                     estat = State.valueOf(st);
                     System.out.print("district: ");
                     dist = read.nextInt();
+                    read.nextLine();
                     p1 = null;
-                    for (MP p : c.getMPs())
-                        if (p.getFullname().equals(fullname) && p.getState().equals(estat) && p.getDistrict() == dist) {
-                            p1 = p;
-                            break;
-                        }
+                    p1 = c.getMP(estat, dist);
                     if (p1.equals(null)) {
                         System.out.println("there's not such MP in the congress");
                         break;
                     }
-                    System.out.println("Enter the info of the attribute you want to change of " + fullname + " (type & value");
+                    System.out.println("Enter the info of the attribute you want to change of " + p1.getFullname() + " (type & value");
                     System.out.print("type of attribute: ");
-                    read.nextLine();
+                    //read.nextLine();
                     def = c.getAttrDef(read.nextLine());
                     System.out.print("new value: ");
-                    read.nextLine();
+                    //read.nextLine();
                     String o = read.nextLine();
                     Boolean found = false;
                     for (Attribute a : p1.getAttributes()) {
-                        System.out.println("hola");
                         if (a.getDefinition().equals(def)) {
                             a.setValue(o);
                             found = true;
@@ -252,7 +243,7 @@ public class WeightAlgDriver {
                     break;
                 case 5:
                     System.out.println("Enter the type: ");
-                    read.nextLine();
+                    //read.nextLine();
                     def = c.getAttrDef(read.nextLine());
                     System.out.println();
                     if(c.hasAttrDef(def)) System.out.println(def);
@@ -261,7 +252,7 @@ public class WeightAlgDriver {
                     break;
                 case 6:
                     System.out.println("Enter the type: ");
-                    read.nextLine();
+                    //read.nextLine();
                     def = c.getAttrDef(read.nextLine());
                     System.out.println("actual importance is "+def.getImportance());
                     if(c.hasAttrDef(def)) {
@@ -275,6 +266,16 @@ public class WeightAlgDriver {
                     seguir = false;
                     break;
             }
+        }
+    }
+    public static void Compute_Weights() {
+        Boolean seguir = true;
+        Scanner read = new Scanner(System.in);
+        while(seguir) {
+            System.out.println("Computing weights:\n");
+            System.out.println();
+            Integer num = read.nextInt();
+            read.nextLine();
         }
     }
 }
