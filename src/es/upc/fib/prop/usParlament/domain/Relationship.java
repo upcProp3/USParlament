16eee517e3cca8b1aed312449b36821a44f6604a
miqueljubert <miqueljubert@gmail.com>
2015-05-01 13:34:51 +0200

    toString changed

--- src/es/upc/fib/prop/usParlament/domain/Relationship.java
+++ src/es/upc/fib/prop/usParlament/domain/Relationship.java
@@ -8,13 +8,24 @@ import es.upc.fib.prop.shared13.Node;
  */
 public class Relationship extends Edge {
 
-	/**
+    @Override
+    public String toString()
+    {
+        MP m1,m2;
+        m1 = (MP)super.getNode();
+        m2 = (MP)super.getNeighbor(getNode());
+        return "("+m1.getState()+" "+m1.getDistrict()+","+m2.getState()+" "+m2.getDistrict()+")";
+    }
+
+    /**
 	 * @param m1
 	 * @param m2
 	 * @param w
 	 * @pre True
 	 * @post An Edge between the nodes n1 and n2 with weight w is created
 	 */
+
+
 	public Relationship(Node m1, Node m2, int w) {
 		super(m1, m2, w);
 	}
